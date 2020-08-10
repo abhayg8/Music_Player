a74fef9640cdc62367c29b2395c3e90b8b9098e8
Abhay Gopinathan <abhay.gopinathan@gmail.com>
2020-08-10 16:58:23 +0530

    New UI colours and images added to buttons

--- GUI/mainwindow.py
+++ GUI/mainwindow.py
@@ -6,6 +6,7 @@ import wave
 import tkinter.filedialog
 import tkinter as tk
 from pygame import mixer
+from PIL import Image, ImageTk
 from gui_math import *
 import time
 from math import floor
@@ -152,8 +153,9 @@ mixer.music.set_volume(0.5)
 
 #Setting up window for program
 window  = tk.Tk()
-window.geometry("800x500")
+window.geometry("800x303")
 window.title("Epic Digital Audio Workstation")
+window.configure(bg='black')
 
 #Setting up song label 
 currentPlayingLabel = tk.StringVar()
@@ -167,7 +169,25 @@ songLengthLabelText.set("--:--")
 
 
 #Generating images for buttons 
-play_photo = tk.PhotoImage(file="C:/Users/abhay\Documents/Projects/Music_Player/images/play_button.png")
+play_photo = Image.open("C:/Users/abhay/Documents/Projects/Music_Player/images/play_button.png")
+play_photo_resize = play_photo.resize((50,37), Image.ANTIALIAS)
+play_photo_tk = ImageTk.PhotoImage(play_photo_resize)
+
+pause_photo = Image.open("C:/Users/abhay/Documents/Projects/Music_Player/images/pause_button.png")
+pause_photo_resize = pause_photo.resize((50,37), Image.ANTIALIAS)
+pause_photo_tk = ImageTk.PhotoImage(pause_photo_resize)
+
+rewind_photo = Image.open("C:/Users/abhay/Documents/Projects/Music_Player/images/rewind_button.png")
+rewind_photo_resize = rewind_photo.resize((50,37), Image.ANTIALIAS)
+rewind_photo_tk = ImageTk.PhotoImage(rewind_photo_resize)
+
+record_photo = Image.open("C:/Users/abhay/Documents/Projects/Music_Player/images/record_button.png")
+record_photo_resize = record_photo.resize((50,37), Image.ANTIALIAS)
+record_photo_tk = ImageTk.PhotoImage(record_photo_resize)
+
+stop_photo = Image.open("C:/Users/abhay/Documents/Projects/Music_Player/images/stop_button.png")
+stop_photo_resize = stop_photo.resize((50,37), Image.ANTIALIAS)
+stop_photo_tk = ImageTk.PhotoImage(stop_photo_resize)
 
 #Used to calibrate the size of pixels in buttons
 pixelVirtual = tk.PhotoImage(width=1,height=1)
@@ -185,13 +205,13 @@ pauseFrame.place(x=100,y=100)
 rewindFrame = tk.Frame(master=window, relief=tk.GROOVE, width=100, height=50, borderwidth=5)
 rewindFrame.place(x=100,y=150)
 
-selectFrame = tk.Frame(master=window, relief=tk.GROOVE, width=300, height=50, borderwidth=5)
+selectFrame = tk.Frame(master=window, relief=tk.GROOVE, width=300, height=50, borderwidth=5, bg='black')
 selectFrame.place(x=100,y=0)
 
-playTimeFrame = tk.Frame(master=window, relief=tk.GROOVE, width=50, height=50, borderwidth=5)
+playTimeFrame = tk.Frame(master=window, relief=tk.GROOVE, width=50, height=50, borderwidth=5, bg='black')
 playTimeFrame.place(x=665, y=50)
 
-songLengthFrame = tk.Frame(master=window, relief=tk.GROOVE, width=50, height=50, borderwidth=5)
+songLengthFrame = tk.Frame(master=window, relief=tk.GROOVE, width=50, height=50, borderwidth=5, bg='black')
 songLengthFrame.place(x=665, y=100)
 
 recordFrame = tk.Frame(master=window, relief=tk.GROOVE, width=50, height=50, borderwidth=5)
@@ -204,48 +224,48 @@ stopRecordFrame.place(x=100, y=250)
 
 
 #Setting up control buttons
-playButton = tk.Button(master=playFrame,text="Play", width=17, height=2)
-playButton.pack()
+playButton = tk.Button(master=playFrame, image=play_photo_tk, width=100, height=37, bg='black')
+playButton.pack(side=tk.TOP)
 playButton.bind("<Button-1>", handle_play)
 
-pauseButton = tk.Button(master=pauseFrame,text="Pause", width=17, height=2)
-pauseButton.pack()
+pauseButton = tk.Button(master=pauseFrame, image=pause_photo_tk, width=100, height=37, bg='black')
+pauseButton.pack(side=tk.TOP)
 pauseButton.bind("<Button-1>", handle_pause)
 
-rewindButton = tk.Button(master=rewindFrame,text="Rewind", width=17, height=2)
-rewindButton.pack()
+rewindButton = tk.Button(master=rewindFrame, image=rewind_photo_tk, width=100, height=37, bg='black')
+rewindButton.pack(side=tk.TOP)
 rewindButton.bind("<Button-1>", handle_rewind)
 
-selectButton = tk.Button(master=selectFrame,text="Choose song", width=17, height=2)
+selectButton = tk.Button(master=selectFrame,text="Choose song", width=17, height=2, bg='black', foreground='white')
 selectButton.pack(side=tk.LEFT)
 selectButton.bind("<Button-1>", handle_select)
-selectLabel = tk.Label(selectFrame, textvariable=currentPlayingLabel, width=85, height=2)
+selectLabel = tk.Label(selectFrame, textvariable=currentPlayingLabel, width=85, height=2, bg='black', foreground='white')
 selectLabel.pack(side=tk.LEFT)
 
-recordButton = tk.Button(master=recordFrame, text="Record", width=17, height=2)
-recordButton.pack(side=tk.LEFT)
+recordButton = tk.Button(master=recordFrame, image=record_photo_tk, width=100, height=37, bg='black')
+recordButton.pack(side=tk.TOP)
 recordButton.bind("<Button-1>", handle_record)
 
-stopRecordButton = tk.Button(master=stopRecordFrame, text="Stop Record", width=17, height=2)
-stopRecordButton.pack(side=tk.LEFT)
+stopRecordButton = tk.Button(master=stopRecordFrame, image=stop_photo_tk, width=100, height=37, bg='black')
+stopRecordButton.pack(side=tk.TOP)
 stopRecordButton.bind("<Button-1>", stop_record)
 
 #Setting up volume slider
-volumeLabel = tk.Label(volumeFrame, text="    Volume", width=11, height=1)
+volumeLabel = tk.Label(volumeFrame, text="    Volume", width=11, height=1, bg='black', foreground='white')
 volumeLabel.pack(side=tk.TOP)
-volumeSlider = tk.Scale(volumeFrame, from_=100, to=0, width=58, length=264, command = set_volume)
+volumeSlider = tk.Scale(volumeFrame, from_=100, to=0, width=58, length=264, command = set_volume, bg='black', foreground='black')
 volumeSlider.set(50)
 volumeSlider.pack(side=tk.TOP)
 
 #Setting up info labels
-playTimeInfo = tk.Label(master=playTimeFrame, text="Time Elapsed:", width=10, height=2)
+playTimeInfo = tk.Label(master=playTimeFrame, text="Time Elapsed:", width=10, height=2, bg='black', foreground='white')
 playTimeInfo.pack(side=tk.LEFT)
-playTimeLabel = tk.Label(master=playTimeFrame, textvariable=playTimeLabelText, width=6, height=1)
+playTimeLabel = tk.Label(master=playTimeFrame, textvariable=playTimeLabelText, width=6, height=1, bg='black', foreground='white')
 playTimeLabel.pack(side=tk.RIGHT)
 
-songLengthInfo = tk.Label(master=songLengthFrame, text="Song Length:", width=10, height=2)
+songLengthInfo = tk.Label(master=songLengthFrame, text="Song Length:", width=10, height=2, bg='black', foreground='white')
 songLengthInfo.pack(side=tk.LEFT)
-songLengthLabel = tk.Label(master=songLengthFrame, textvariable=songLengthLabelText, width=6, height=1)
+songLengthLabel = tk.Label(master=songLengthFrame, textvariable=songLengthLabelText, width=6, height=1, bg='black', foreground='white')
 songLengthLabel.pack(side=tk.RIGHT)
 
 playButton.after(1000,duration_control)
